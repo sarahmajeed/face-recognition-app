3fc0d020e4269ff19d8d89779a8da1029dc12432
Sarahmdawood <sarahmajeed368@gmail.com>
2020-06-20 16:35:07 +0500

    Designing the homepage

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -4,6 +4,11 @@ import './HomePage.scss';
 const HomePage = ({ handleHomepageSignin, handleHomepageRegister }) => {
   return (
     <div className='home'>
+      <div className="link">
+        <a target="_blank" href="https://github.com/Sarahmdawood"><i class="fa fa-github fa-2x"></i></a>
+        <a href="https://www.linkedin.com/in/sarahmajeed/"><i class="fab fa-linkedin fa-2x"></i></a>
+        <a href="https://www.facebook.com/sarah.majeed.507/"><i class="fab fa-facebook fa-2x"></i></a>
+      </div>
       <h1>
         <i>Want to get your face detected?</i>
       </h1>
@@ -13,6 +18,10 @@ const HomePage = ({ handleHomepageSignin, handleHomepageRegister }) => {
       <button className='btn' onClick={handleHomepageSignin}>
         Signin
       </button>
+      <div className="source-code">
+
+        <a target="_blank" href="https://github.com/Sarahmdawood/face-recognition-app"><i class='fa fa-github fa-2x'></i><p>View Source Code</p></a>
+      </div>
     </div>
   );
 };
